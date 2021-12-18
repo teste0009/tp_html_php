6812c7d80132ef5681fa362d8bff35c3a13853c2
teste0009 <maidentallica@gmail.com>
2021-12-18 02:46:07 -0300

    User

--- controllers/App.php
+++ controllers/App.php
@@ -7,8 +7,6 @@ class App extends Router {
 
 	public User $user;
 
-	/**
-	 */
 	function __construct() {
 		parent::__construct();
 		$this->user = new User();
@@ -20,8 +18,7 @@ class App extends Router {
 
 	public function response() {
 		if ($this->is_route_registered($this->request_route)) { // http_response_code(200);
-			header('HTTP/1.1 200 OK');
-			// echo(__DIR__.'<br/>');
+			header('HTTP/1.1 200 OK'); // echo(__DIR__.'<br/>');
 			require_once(__DIR__.'/'.$this->_routes_registered[$this->request_route].'.php');
 		}
 		else {
--- controllers/User.php
+++ controllers/User.php
@@ -3,15 +3,26 @@ namespace Controllers;
 use \Models\Db;
 
 class User extends Db {
-
+  private string $nombre = "";
+  private string $email = "";
+  private bool $bIsLogged = false;
 
   function __construct() {
     parent::__construct();
     echo 'Class User.<br/>';
   }
 
+  function getNombre(): string {
+    return $this->nombre;
+  }
+
+  function getEmail(): string {
+    return $this->email;
+  }
+
+  function isLogged(): bool {
+    return $this->bIsLogged;
+  }
 }
-
-
 
 ?>
